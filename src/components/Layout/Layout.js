2ced9d8ee41e20a3853fe54136a060ba0b8f4774
ellie <ellie.tms1371@gmail.com>
2020-08-08 11:45:54 +0100

    use higher-order component  (HOC)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar,sideDrawer,Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
 
-// const layout = () => (
-//     <div>
 
-//     </div>
-
-// );
+export default layout;
